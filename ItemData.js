1779af09359a5c6865e77372887842e438be4da9
FranOttone <77749241+FranOttone@users.noreply.github.com>
2021-06-28 18:29:47 -0300

    Add files via upload

---
+++ ItemData.js
@@ -0,0 +1,8 @@
+export const itemsData=
+[
+{id:1,name:"SWEATER ASPEN",price:5200,img:"https://res.cloudinary.com/fran02/image/upload/c_scale,h_400,w_350/v1624563254/aspen_mmpasf.jpg"},
+{id:2,name:"CHALECO TEXAS",price:2580,img:"https://res.cloudinary.com/fran02/image/upload/c_scale,h_400,w_350/v1624563254/texas_wgisqe.jpg"},
+{id:3,name:"CAMISA VIENA ",price:3280,img:"https://res.cloudinary.com/fran02/image/upload/c_scale,h_400,w_350/v1624563254/viena_rqn5gv.jpg"},
+{id:4,name:"BUZO NEW YORK ",price:3360,img:"https://res.cloudinary.com/fran02/image/upload/c_scale,h_400,w_350/v1624563254/new_york_zetzc2.jpg"}
+]
+ 
